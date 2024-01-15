6984eecc246b8213d8b6d52490e902c4594315c2
Sergio Gabriel <sergiogscruz@gmail.com>
2024-01-15 08:32:40 -0300

    decrypt consumindo do input

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 var CryptoJS = require("crypto-js");
 
-var KEY = '123321';
+var KEY = '123';
 var PLAINTEXT = 'Esta é uma mensagem para encripta';
+var CIPHERTEXT = 'U2FsdGVkX1+X1zKgP+67b8mwHsaR4/uBuT5xt1EztEg=';
 
-var ciphertext = CryptoJS.AES.encrypt(PLAINTEXT, KEY).toString();
-var bytes = CryptoJS.AES.decrypt(ciphertext, KEY);
-var originalText = bytes.toString(CryptoJS.enc.Utf8);
+// var ciphertext = CryptoJS.AES.encrypt(PLAINTEXT, KEY).toString();
+var originalText = CryptoJS.AES.decrypt(CIPHERTEXT, KEY).toString(CryptoJS.enc.Utf8);
 
-console.log('CIPHERTEXT: ', ciphertext)
+// console.log('CIPHERTEXT: ', ciphertext)
 console.log('ORIGINALTEXT: ', originalText)
